c7cc6e505bc953a2b41621e24efa20b4268976f1
Arash Horoufi <6829077+ahoroufi@users.noreply.github.com>
2025-10-01 19:38:07 -0700

    fix style by adding id - Storing Component Syle Files Next to Components

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,15 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Core Concepts</h2>
-        <ul>
-          <CoreConcept {...CORE_CONCEPTS[0]}/>
-          <CoreConcept {...CORE_CONCEPTS[1]}/>
-          <CoreConcept {...CORE_CONCEPTS[2]}/>
-          <CoreConcept {...CORE_CONCEPTS[3]}/>
-        </ul>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+            <ul>
+              <CoreConcept {...CORE_CONCEPTS[0]}/>
+              <CoreConcept {...CORE_CONCEPTS[1]}/>
+              <CoreConcept {...CORE_CONCEPTS[2]}/>
+              <CoreConcept {...CORE_CONCEPTS[3]}/>
+            </ul>
+        </section>
       </main>
     </div>
   );
