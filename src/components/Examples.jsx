5f3c3a30986d1b269dd0f9ec254fa464f33009d6
Arash Horoufi <6829077+ahoroufi@users.noreply.github.com>
2025-10-13 18:44:38 -0700

    69.Setting Default Prop Values

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import TabButton from './TabButton';
 import Section from "./Section";
+import Tabs from "./Tabs";
 import { EXAMPLES } from "../data";
 
 
@@ -27,13 +28,15 @@ function Examples(){
   }
     return (
         <Section title="Examples" id="examples">
-            <menu>
-                <TabButton isSelected={selectedTopic === 'components'} onClick={()=>handleSelect('components')}>Components</TabButton>
-                <TabButton isSelected={selectedTopic === 'jsx'} onClick={()=>handleSelect('jsx')}>JSX</TabButton>
-                <TabButton isSelected={selectedTopic === 'props'} onClick={()=>handleSelect('props')}>Props</TabButton>
-                <TabButton isSelected={selectedTopic === 'state'} onClick={()=>handleSelect('state')}>State</TabButton>
-            </menu>
-            {tabContent}
+          <Tabs buttons={
+            <>
+              <TabButton isSelected={selectedTopic === 'components'} onClick={()=>handleSelect('components')}>Components</TabButton>
+              <TabButton isSelected={selectedTopic === 'jsx'} onClick={()=>handleSelect('jsx')}>JSX</TabButton>
+              <TabButton isSelected={selectedTopic === 'props'} onClick={()=>handleSelect('props')}>Props</TabButton>
+              <TabButton isSelected={selectedTopic === 'state'} onClick={()=>handleSelect('state')}>State</TabButton>
+            </>
+          }/>
+          {tabContent}         
         </Section>
     )
 }
